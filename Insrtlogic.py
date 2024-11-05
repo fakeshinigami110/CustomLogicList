0b84e5a5c03bd21da431eceb286590578e56e209
fakeshinigami110 <yaghoubi.a.sa@gmail.com>
2024-11-05 20:05:13 +0330

    first test

--- Insrtlogic.py
+++ Insrtlogic.py
@@ -1,38 +1,23 @@
-'''
+class CustomList(list):
 
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
+    def Insert(self, indexNum, value):
+        arrayCopy = []
+        length = len(self)
+        inIndex = 0
 
-'''
+        for item in self:
+            if indexNum == inIndex:
+                arrayCopy.append(value)
 
-ali = ['2','4']
-ali.insert(1,'11')
-# outpot = [2,11,4]
-print (ali)
+            arrayCopy.append(item)
+            inIndex += 1
 
-indexNum = 5
-value = 55
-array = [1,2,3,4]
-arrayCopy =[]
-length =  len(array)
-inInddex = 0
-for _ in array :
-  
-    if indexNum == inInddex :
-        arrayCopy.append(value)
-        arrayCopy.append(_)
-        print (f"if yes indexNum :{indexNum} inInddex ={inInddex} lenth = {length}  _ = {_} arrayCopy = {arrayCopy}")
-    else :
-        arrayCopy.append(_)
-        print (f"if else indexNum :{indexNum} inInddex ={inInddex} lenth = {length}  _ = {_} arrayCopy = {arrayCopy}")
-        
-    inInddex =inInddex+ 1
-    
-print (f"end of loop indexNum :{indexNum} inInddex ={inInddex} lenth = {length}  _ = {_} arrayCopy = {arrayCopy}")
+        if indexNum >= length:
+            arrayCopy.append(value)
 
-if indexNum > length :
-    arrayCopy.append(value)
+        self.clear()
+        self.extend(arrayCopy)
 
-
-print(arrayCopy)
+array = CustomList([1, 2, 3, 4])
+array.Insert(1, 10)
+print(array)
---
+++ test.py
@@ -0,0 +1,6 @@
+# from Insrtlogic import array
+
+array = [1,2,3]
+array2 = [1,1,1]
+array = array2
+print(array)
